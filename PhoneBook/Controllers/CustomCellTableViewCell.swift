088479b7bfcc1d3987c24a0decbc2c87fad527e3
Alex <y100b@ya.ru>
2020-10-21 20:31:30 +0500

    not work'g segue %(

--- PhoneBook/Controllers/CustomCellTableViewCell.swift
+++ PhoneBook/Controllers/CustomCellTableViewCell.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class CustomCellTableViewCell: UITableViewCell {
+@IBDesignable class CustomCellTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var phoneNumLabel: UILabel!
     @IBOutlet weak var characterLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,7 +23,15 @@ class CustomCellTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+
+    
     @IBAction func moreInfoButtonTapped(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let aboutViewController = storyBoard.instantiateViewController(identifier: "about") as! AboutViewController
+        else { return }
+        aboutViewController.nibName = "about"
+        show(aboutViewController, sender: nil)
     }
     
 }
